61ec885cf6b809dcec22bf9f9a7f2113af63f197
Max Solomčuk <solomchuk@users.noreply.github.com>
2020-07-25 17:59:19 +0100

    Initial set of changes: keep files relevant for CentOS with IceWM, remove the rest. Tweaks to some install scripts to work with CentOS 6 repos. Workaround in the websockify/run script for Python 2.6 support.

--- src/centos/install/tigervnc.sh
+++ src/centos/install/tigervnc.sh
@@ -2,6 +2,6 @@
 set -e
 
 echo "Install TigerVNC server"
-wget https://dl.bintray.com/tigervnc/stable/tigervnc-el7.repo -O /etc/yum.repos.d/tigervnc.repo
+wget https://dl.bintray.com/tigervnc/stable/tigervnc-el6.repo -O /etc/yum.repos.d/tigervnc.repo
 yum -y install tigervnc-server
 yum clean all
